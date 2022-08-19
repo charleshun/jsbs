9f4cda876397e71ba7911f7d0914ebf10562b52f
RBS <ronai.balint@naih.hu>
2022-08-19 14:08:23 +0200

    bill and sum function

--- sandbox.js
+++ sandbox.js
@@ -1,5 +1,34 @@
 let age =27;
 let year = 2019;
 
-alert('hello');
+//alert('hello');
 console.log(year, age);
+
+const sum = (a,b) => {
+    return a+b;
+}
+//module.exports = sum; //jest not run
+
+//const ul = document.querySelector('.people');
+const names = ['shaun','mario','luigi','ryu'];
+
+let html = ``;
+
+names.forEach((name) => {
+    html+= `<li style="">${name}</li>`;
+});
+console.log(html);
+
+//console.log(ul);
+
+const bill = function(products, tax){
+    let total = 0;
+    for(let i = 0; i < products.length; i++){
+        total += products[i] + products[i] * tax;
+    }
+    return total;
+    }
+   
+    console.log('bill:'+bill([10,15,30], 0.2));
+
+
